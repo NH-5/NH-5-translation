34f31f9219f6b2d064112475b3fc039a1926e575
NH5 <NH5wuzheng@outlook.com>
2025-11-12 21:35:41 +0800

    write model service

--- model_service/README.md
+++ model_service/README.md
@@ -2,4 +2,4 @@
 
 模型文件下载自[modelscope](https://www.modelscope.cn/),存放于[相应文件夹中](model/qwen3-0.6b/)
 
-鉴于safetensors文件过大,这里将它没有上传GitHub仓库.
+由于safetensors文件过大,这里将它没有上传GitHub仓库.
---
+++ model_service/modelservice.py
@@ -0,0 +1,49 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def modelservice(input_prompt:str):
+
+    model_path = "model_service/model/qwen3-0.6b"
+
+    # load the tokenizer and the model
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        dtype="auto",
+        device_map="auto"
+    )
+
+    # prepare the model input
+    prompt = input_prompt
+    messages = [
+        {"role": "user", "content": prompt}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+        enable_thinking=False # Switches between thinking and non-thinking modes. Default is True.
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+    # conduct text completion
+    generated_ids = model.generate(
+        **model_inputs,
+        max_new_tokens=32768
+    )
+    output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+
+    # parsing thinking content
+    try:
+        # rindex finding 151668 (</think>)
+        index = len(output_ids) - output_ids[::-1].index(151668)
+    except ValueError:
+        index = 0
+
+    
+    content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+    return content
+
+if __name__ == '__main__':
+    content = modelservice("Hello Who are you")
+    print(content)
