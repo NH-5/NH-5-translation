670efa769e2984ff63f6e788c56311e5edd65374
NH5 <NH5wuzheng@outlook.com>
2025-11-13 14:02:44 +0800

    write model service with fastapi

--- model_service/modelservice.py
+++ model_service/modelservice.py
@@ -1,21 +1,30 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
 
-def modelservice(input_prompt:str):
+class RequestData(BaseModel):
+    role:str
+    content:str
+    max_new_tokens:int =32768
 
-    model_path = "model_service/model/qwen3-0.6b"
+app = FastAPI()
+model_path = os.path.abspath("model/qwen3-0.6b")
 
-    # load the tokenizer and the model
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(
+# load the tokenizer and the model
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(
         model_path,
         dtype="auto",
         device_map="auto"
-    )
+)
+
+@app.post('/generate')
+def generate(data:RequestData):
 
     # prepare the model input
-    prompt = input_prompt
     messages = [
-        {"role": "user", "content": prompt}
+        {"role": data.role, "content": data.content}
     ]
     text = tokenizer.apply_chat_template(
         messages,
@@ -28,15 +37,24 @@ def modelservice(input_prompt:str):
     # conduct text completion
     generated_ids = model.generate(
         **model_inputs,
-        max_new_tokens=32768
+        max_new_tokens=data.max_new_tokens
     )
     output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
 
     index = 0
     content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
 
-    return content
+    return {'response':content}
+
+@app.get('/')
+def test():
+    return 'hello world'
 
 if __name__ == '__main__':
-    content = modelservice("Hello Who are you")
-    print(content)
+    import requests
+    inputs = {
+        'role':'user',
+        'content':'Hello who are you?'
+    }
+    resp = requests.post('http://localhost:8000/generate',json=inputs)
+    print(resp.json())
